dd647478a5a1d9bc677e1f24f2b93d41b404683f
Ander-Lr <andersonllr4@gmail.com>
2026-04-30 17:11:27 -0500

    feat: Creacion de modelo  Producto

---
+++ src/main/java/com/P1/Lab1/model/Producto.java
@@ -0,0 +1,30 @@
+package com.P1.Lab1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String categoria;
+
+    private Double precio;
+
+    private Integer stock;
+
+    private Boolean activo;
+}
